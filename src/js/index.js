e8fec1a829a2be4de2cfe8f95eeea07af58b7627
CommanderSant <santoslozanocamiloandres@hotmail.com>
2019-09-20 23:59:48 +0000

    excusegenerator

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-
 import "file-loader?name=[name].[ext]!../index.html";
 import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
@@ -7,5 +6,17 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
+
+function excuseGenerator() {
+  let person = ["my mom", "my father", "a thieft", "a stranger"];
+  let what = ["took ", "stole ", "lost", "picked up"];
+  let when = ["this morning", "this afternoon", "yesterday", "last night"];
+
+  let perIndex = Math.floor(Math.random() * person.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return person[perIndex] + " " + what[whatIndex] + " " + when[whenIndex];
+}
